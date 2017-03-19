3602f541bd9ed8d6e330559b399800049f86edbe
Prateek Gogia <gogia.prateek@hotmail.com>
2017-03-19 15:56:53 -0400

    Updated a little javascript for complaint.js

--- js/complaint.js
+++ js/complaint.js
@@ -2,9 +2,12 @@ function openNav() {
     document.getElementById("mySideNav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("postComplaint").style.backgroundColor = "rgba(0,0,0,0.4)"; document.getElementById("complaint").style.marginLeft = "100px";
 }
 function closeNav() {
     document.getElementById("mySideNav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
+    document.getElementById("postComplaint").style.backgroundColor = "white";
+     document.getElementById("complaint").style.marginLeft = "0px";
 }
