1fbac71610b4f27e39a6b750d8add024e11c1e69
Prateek Gogia <gogia.prateek@hotmail.com>
2017-04-23 04:27:56 +0530

    Added Angular.js

--- js/dsw.js
+++ js/dsw.js
@@ -2,12 +2,17 @@ function openNav() {
     document.getElementById("mySideNav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    document.getElementById("postComplaint").style.backgroundColor = "rgba(0,0,0,0.4)"; document.getElementById("complaint").style.marginLeft = "100px";
+    document.getElementById("postComplaint").style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("complaint").style.marginLeft = "300px";
 }
 function closeNav() {
     document.getElementById("mySideNav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
     document.getElementById("postComplaint").style.backgroundColor = "white";
-     document.getElementById("complaint").style.marginLeft = "0px";
+    document.getElementById("complaint").style.marginLeft = "auto";
+}
+function text() {
+    var text = document.getElementById("postComplaint").value;
+    return text;
 }
