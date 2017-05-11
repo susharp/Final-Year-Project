a2dae5abbf0519595a4cab7a14d576b258152f49
Prateek Gogia <gogia.prateek@hotmail.com>
2017-05-12 01:09:35 +0530

    Added complaint.js

--- js/complaint.js
+++ js/complaint.js
@@ -14,6 +14,29 @@ function closeNav() {
     document.getElementById("complaint").style.marginLeft = "auto";
     document.getElementById("form").style.marginLeft = "auto";
 }
-function clear() {
-
+function member(s1, s2) {
+    var s1 = document.getElementById(s1);
+    var s2 = document.getElementById(s2);
+    s2.innerHTML = "";
+    if(s1.value == "Mess" || s1.value == "Food Trucks" || s1.value == "Transport") {
+        s2.innerHTML = "Gp. Capt. (Retd.) Ajoy Mudaliar (Director)";
+    }
+    else if (s1.value == "Hostel") {
+        s2.innerHTML = "Col. (Retd.) Prahlad Singh (Director-Hostels)";
+    }
+    else if(s1.value == "IT") {
+        s2.innerHTML = "Mr. Ravi Kant Pachauri (Manager-IT)";
+    }
+    else if(s1.value == "Fees and Accounts") {
+        s2.innerHTML = "Mr. Sunil Kumar Bhargava (Manager)";
+    }
+    else if(s1.value == "Library") {
+        s2.innerHTML = "Mr. Sonu Kumar (Deputy Librarian)";
+    }
+    else if(s1.value == "MI Room") {
+        s2.innerHTML = "Dr. C.P Bandhu (Medical Head)";
+    }
+    else if(s1.value == "SRC") {
+        s2.innerHTML = "Mr. Sudhir Kumar Mishra (Resource head)";
+    }
 }
